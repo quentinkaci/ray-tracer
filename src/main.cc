c1c40602759e2a69a1baac2ad46c156c80c9e80d
Quentin Kaci <quentin.kaci@lrde.epita.fr>
2021-03-01 12:45:19 +0100

    up(main): modify scene

--- src/main.cc
+++ src/main.cc
@@ -17,13 +17,13 @@ int main(void)
 {
     Scene scene(Camera(Point3(0., 0., 0.), Point3(0., 0., 1.), Point3(0., 1., 0.), 90., 60., 1.));
 
-    UniformTexture sphere_uniform_texture_red(TextureMaterialCaracteristics{0.007, 0.30, 1., 0.3, Color(200, 0, 0)});
+    UniformTexture sphere_uniform_texture_red(TextureMaterialCaracteristics{0.005, 0.30, 1., 0.30, Color(150, 0, 0)});
     scene.objects.emplace_back(new Sphere(sphere_uniform_texture_red, Point3(2., 0., 5.), 1.7));
 
-    UniformTexture sphere_uniform_texture_green(TextureMaterialCaracteristics{0.007, 0.30, 1., 0.5, Color(0, 255, 0)});
+    UniformTexture sphere_uniform_texture_green(TextureMaterialCaracteristics{0.005, 0.30, 1., 0.30, Color(0, 0, 150)});
     scene.objects.emplace_back(new Sphere(sphere_uniform_texture_green, Point3(-2., 0., 5.), 1.7));
 
-    scene.light_sources.emplace_back(new PointLight(Point3(-10., 20., -10.), Color(255, 255, 255), 2.));
+    scene.light_sources.emplace_back(new PointLight(Point3(10., 10., -10.), Color(255, 255, 255), 2.));
 
     Engine engine(scene);
     Image image = engine.run(1080, 1920);
